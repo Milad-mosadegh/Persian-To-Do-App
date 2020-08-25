c00f4df19fe23e998d757dbb34355ee06ea86fb2
Milad Mosadegh <miladmosadegh@Milads-MBP.fritz.box>
2020-08-25 10:47:50 +0200

    Updates

---
+++ precache-manifest.cf23e82cd5c7771b7fbe604ef628d895.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21004e3a23bcf230086dac88be937426",
+    "url": "/Persian-To-Do-App/index.html"
+  },
+  {
+    "revision": "28e8c5612966b8bc476c",
+    "url": "/Persian-To-Do-App/static/js/2.a05dc142.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Persian-To-Do-App/static/js/2.a05dc142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5bc12fb766b3855b8d",
+    "url": "/Persian-To-Do-App/static/js/main.9059b28c.chunk.js"
+  },
+  {
+    "revision": "43e0e676dbdcbaaea657",
+    "url": "/Persian-To-Do-App/static/js/runtime-main.234f57e9.js"
+  }
+]);
